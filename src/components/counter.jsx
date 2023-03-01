cd6fa49641d00081f9e0f3afe13c2ee71ea779ec
Ivan <ivanwong810520@gmail.com>
2023-03-01 15:37:19 +0800

    Add formatCount() function
    If count is 0, the return result would be `zero`, else is numeric value.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,11 +7,17 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
         <h1>Hello World</h1>
       </div>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+    // return html expression is allowed
+    // return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
