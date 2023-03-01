fd2a7f3c40221a12ba1c282e3248f65fc64b4861
Ivan <ivanwong810520@gmail.com>
2023-03-01 15:47:11 +0800

    Add class name and styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,18 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
   };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge bg-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
         <h1>Hello World</h1>
       </div>
     );
