361f5c3391dd3a5a48a0164fff5dd4b9ad98ad44
Ivan <ivanwong810520@gmail.com>
2023-03-01 15:43:26 +0800

    Add an image element
    `https://picsum.photos/200` generate random 200 x 200 pixel images.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
     return (
       <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
         <h1>Hello World</h1>
