ed1fea598d2f42c4063206b4ce66b501102cd96d
Ivan <ivanwong810520@gmail.com>
2023-03-01 17:19:08 +0800

    Remove unnecessary code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,27 +9,13 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map((tag, index) => (
             <li key={index}>{tag}</li>
           ))}
         </ul>
-        <h1>Hello World</h1>
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
