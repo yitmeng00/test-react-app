1322a0c23b26393924dda3fa8ac782e3c08a08be
Ivan <ivanwong810520@gmail.com>
2023-03-01 18:00:53 +0800

    Update counter.jsx
    Explanation for undefined result return by `this`.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,9 +6,11 @@ class Counter extends Component {
   };
 
   handleIncrement() {
-    // this.state.count is undefined
-    // console.log("Increment Clicked", this.state.count);
-    console.log("Increment Clicked");
+    // In this function, it does not has access to this
+    // Depends on how a funciton is called, this can be reference to different object
+    // obj.method(); if a function is called as part of a method in an object, this will always return a reference to that object
+    // function(); if the function is a standalone function without an object reference, this by default returns a reference to the iwndow object but if the strict mode is enabled this will return undefined
+    console.log("Increment Clicked", this.state.count);
   }
 
   render() {
@@ -21,7 +23,6 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <h1>Hello World</h1>
       </div>
     );
   }
