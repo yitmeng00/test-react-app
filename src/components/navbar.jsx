fb62933170b648e2dae1877c38dc35d073b6f74d
Ivan <ivanwong810520@gmail.com>
2023-03-02 14:53:16 +0800

    Added object destructuring
    Object Destructuring is to destructure so that no need repeat `props` argument several times.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,9 @@
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a href="#" className="navbar-brand">
         Navbar{" "}
-        <span className="badge badge-pill bg-secondary">
-          {props.totalCounters}
-        </span>
+        <span className="badge badge-pill bg-secondary">{totalCounters}</span>
       </a>
     </nav>
   );
