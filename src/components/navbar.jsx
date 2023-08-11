a7b2a4fa1e5888da5ee8d50e17ad055a65529b84
Ivan <ivanwong810520@gmail.com>
2023-08-11 11:46:25 +0800

    Added comments in navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,3 +1,5 @@
+// Destructuring props and state, instead of passing param `props` and return `props.totalCounters`, can just pass in {totalCounters}
+// or u can pass the param `props` then add const {totalCounters} = props above the return
 const NavBar = ({ totalCounters }) => {
     // lifecycle hooks is not working in functional component
     console.log("NavBar - Rendered");
